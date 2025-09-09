7176b8050b22e0729aa2047c2685dc1b2ccce6ab
Ka334408 <104441539+Ka334408@users.noreply.github.com>
2025-09-09 12:24:35 +0300

    .

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -27,13 +27,13 @@ export default function HomePage() {
       <div className="text-center bg-white p-8 rounded-xl shadow-md">
         <h1 className="text-3xl font-bold text-blue-600 mb-4">Hello</h1>
         {email && <p className="text-xl text-gray-700 mb-6">{email}</p>}
-
+        {email &&
         <button
           onClick={handleLogout}
           className="bg-red-500 text-white px-6 py-2 rounded-full hover:bg-red-600 transition"
         >
           Logout
-        </button>
+        </button>}
       </div>
     </main>
   );
